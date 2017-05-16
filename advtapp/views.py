bf47a721eedbccffb74f6bb509066ecc630ed75c
Aule <maszs@wp.pl>
2017-05-16 21:35:47 +0200

    Basic correction

--- advtapp/views.py
+++ advtapp/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, absolute_import, print_function
 from django.utils.encoding import python_2_unicode_compatible
 
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from .forms import advertisingForms
 from .models import Advertisement
@@ -11,10 +12,9 @@ from .models import Advertisement
 def advertisingAdd(request):
     if request.method == 'POST':
         form = advertisingForms(request.POST)
-        if form > 0:
-            if form.is_valid():
-                form.save()
-                return HttpResponseRedirect('/lista_ogloszen')
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('list'))
     else:
         form = advertisingForms()
     return render(request, 'advtapp/form_view.html', {'form': form})
